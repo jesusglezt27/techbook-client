978fd8b66563424d4237e0d2620782726fc794af
Jesus Gonzalez <jesusglezt27@gmail.com>
2023-09-27 17:56:12 -0600

    dones

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
 import HomePage from './pages/HomePage';
-import SignupPage from './pages/SignupPage';
-import LoginPage from './pages/LoginPage';
-import ProfilePage from './pages/ProfilePage';
 import Navbar from "./components/home/Navbar";
 
 const App = () => {
@@ -17,10 +14,6 @@ const App = () => {
       <Navbar/>
     <Routes>
       <Route path='/' element={<HomePage/>} />
-      <Route path='/profile' element={<ProfilePage/>} />
-      <Route path='/signup' element={<SignupPage/>} />
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='/login' element={<LoginPage />} />
       <Route path='/home' element={<Home />} />
       <Route path='/books/create' element={<CreateBook />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
