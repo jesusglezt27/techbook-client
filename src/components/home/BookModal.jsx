190fb29c9f1b4074238ac6e18ae4fb71a20cb8a0
Jesus Gonzalez <jesusglezt27@gmail.com>
2023-10-15 19:08:38 -0600

    lorem

--- src/components/home/BookModal.jsx
+++ src/components/home/BookModal.jsx
@@ -32,7 +32,6 @@ const BookModal = ({ book, onClose }) => {
           <BiUserCircle className='text-red-300 text-2xl' />
           <h2 className='my-1'>{book.author}</h2>
         </div>
-        <p className='mt-4'>Anything You want to show</p>
         <p className='my-2'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
           voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
